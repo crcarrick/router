6f47f9edb05001b7171ae5c83cbd308f493dc37e
Chris Carrick <chris@crcarrick.dev>
2024-02-19 22:05:03 -0500

    Update example generator

--- plopfile.mjs
+++ plopfile.mjs
@@ -2,6 +2,55 @@ import cp from 'node:child_process'
 import path from 'node:path'
 import process from 'node:process'
 
+const EXAMPLE_INDEX_HTML = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{name}}</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.tsx"></script>
+  </body>
+</html>
+`.trim()
+
+const EXAMPLE_MAIN_TSX = `
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import App from './App.js'
+
+const root = document.getElementById('root')
+
+if (!root) {
+  throw new Error('Root element not found')
+}
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+`.trim()
+
+const EXAMPLE_APP_TSX = `
+export default function App() {
+  return <h1>Example: {{name}}</h1>
+}
+`.trim()
+
+const EXAMPLE_VITE_CONFIG = `
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
+`.trim()
+
 const PACKAGE_JSON = `
 {
   "name": "{{name}}",
@@ -18,10 +67,9 @@ const PACKAGE_JSON = `
   ],
   "type": "module",
   "scripts": {
-    "build": "pnpm clean && tsc -p tsconfig.json",
-    "clean": "rm -rf ./dist && rm -rf tsconfig.tsbuildinfo",
-    "tests": "",
-    "watch": "pnpm clean && tsc -p tsconfig.json --watch"
+    {{#each scripts}}
+    "{{name}}": "{{command}}"{{#unless @last}},{{/unless}}
+    {{/each}}
   },
   {{#each dependencies}}
   "{{@key}}": {
@@ -80,6 +128,18 @@ export default function (
           dependencies: {
             devDependencies: [{ name: 'typescript', version: '^5.3.3' }],
           },
+          scripts: [
+            { name: 'build', command: 'pnpm clean && tsc -p tsconfig.json' },
+            {
+              name: 'clean',
+              command: 'rm -rf ./dist && rm -rf tsconfig.tsbuildinfo',
+            },
+            { name: 'tests', command: '' },
+            {
+              name: 'watch',
+              command: 'pnpm clean && tsc -p tsconfig.json --watch',
+            },
+          ],
         },
       },
       {
@@ -101,7 +161,21 @@ export default function (
       },
     ],
     actions: [
-      { type: 'add', path: 'examples/{{name}}/src/index.ts' },
+      {
+        type: 'add',
+        path: 'examples/{{name}}/src/main.tsx',
+        template: EXAMPLE_MAIN_TSX,
+      },
+      {
+        type: 'add',
+        path: 'examples/{{name}}/src/App.tsx',
+        template: EXAMPLE_APP_TSX,
+      },
+      {
+        type: 'add',
+        path: 'examples/{{name}}/index.html',
+        template: EXAMPLE_INDEX_HTML,
+      },
       {
         type: 'add',
         path: 'examples/{{name}}/package.json',
@@ -109,21 +183,30 @@ export default function (
         data: {
           dependencies: {
             dependencies: [
+              { name: '@router/router', version: 'workspace:*' },
               { name: 'react', version: '^18.2.0' },
               { name: 'react-dom', version: '^18.2.0' },
             ],
             devDependencies: [
               { name: '@types/react', version: '^18.2.57' },
               { name: '@types/react-dom', version: '^18.2.19' },
+              { name: '@vitejs/plugin-react', version: '^4.2.1' },
               { name: 'typescript', version: '^5.3.3' },
+              { name: 'vite', version: '^5.1.1' },
             ],
           },
+          scripts: [{ name: 'start', command: 'vite' }],
         },
       },
       {
         type: 'add',
         path: 'examples/{{name}}/tsconfig.json',
         template: TSCONFIG_JSON,
+      },
+      {
+        type: 'add',
+        path: 'examples/{{name}}/vite.config.ts',
+        template: EXAMPLE_VITE_CONFIG,
       },
       { type: 'install', dest: 'examples' },
     ],
